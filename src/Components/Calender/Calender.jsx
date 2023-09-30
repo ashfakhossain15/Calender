1e2480ad9378f649d0946a8a6a713677a245ec56
Ashfak Hossain <ashfakhossain@Ashfaks-MacBook-Pro.local>
2023-09-30 22:39:16 +0600

    import calendar and implement it

--- src/Components/Calender/Calender.jsx
+++ src/Components/Calender/Calender.jsx
@@ -1,9 +1,17 @@
-const Calender = () => {
+import { useState } from "react";
+import Calendar from "react-calendar";
+const CalenderJs = () => {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (date) => {
+    setDate(date);
+  };
   return (
     <div>
       <h1>There is the Calender</h1>
+      <div> <Calendar onChange={onChange} value={date}/> </div>
     </div>
   );
 };
 
-export default Calender;
+export default CalenderJs;
