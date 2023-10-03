677b316a930784a5b328e85475bc0e86649fd570
Ashfak Hossain <ashfakhossain@Ashfaks-MacBook-Pro.local>
2023-10-03 21:26:52 +0600

    add Clock

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -1,10 +1,41 @@
+import  { useState, useEffect } from 'react';
+
 
 const Clock = () => {
-    return (
-        <div>
-            <h1>There Is the clock</h1>
-        </div>
-    );
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTimeUnit = (unit) => {
+    return unit < 10 ? `0${unit}` : unit.toString();
+  };
+
+  const hours = formatTimeUnit(time.getHours());
+  const minutes = formatTimeUnit(time.getMinutes());
+  const seconds = formatTimeUnit(time.getSeconds());
+
+  return (
+    <div className="flex">
+      <div className="clock-digit">
+        {hours[0]}
+        <div className="flip">{hours[1]}</div>
+      </div>
+      <div className="clock-digit">
+        {minutes[0]}
+        <div className="flip">{minutes[1]}</div>
+      </div>
+      <div className="clock-digit">
+        {seconds[0]}
+        <div className="flip">{seconds[1]}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Clock;
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -5,7 +5,9 @@ const Main = () => {
   return (
     <section className="relative flex flex-col justify-center items-center min-h-[100vh] bg-[#161623] w-full text-white overflow-hidden ">
       <div className=" relative z-[1000] ">
-        <Clock />
+        <div className="flex justify-center items-center">
+          <Clock />
+        </div>
         <div className="container border-r-[rgba(255,255,255,0.2)] border-b-[rgba(255,255,255,0.2)] relative lg:w-[560px] lg:h-[490px] flex rounded-md bg-gray-500 bg-opacity-30 backdrop-blur-lg border-gray-400 border items-center justify-center ">
           <CalenderJs />
         </div>
